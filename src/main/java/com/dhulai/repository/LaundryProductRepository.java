d474ebb0cdb0e0522e7ed84a8f1442b9d5e6fa1d
Triparna Das <triparnadas153@gmail.com>
2024-03-14 01:25:16 +0530

    small changes

--- src/main/java/com/dhulai/repository/LaundryProductRepository.java
+++ src/main/java/com/dhulai/repository/LaundryProductRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dhulai.entity.LaundryShop;
 import com.dhulai.entity.LaundryShopProducts;
+import com.dhulai.model.LaundryShopProductsId;
 
-public interface LaundryProductRepository extends JpaRepository<LaundryShopProducts, Long> {
+public interface LaundryProductRepository extends JpaRepository<LaundryShopProducts, LaundryShopProductsId> {
     List<LaundryShopProducts> findByLaundryShop(LaundryShop laundryShop);
 }
--- src/main/java/com/dhulai/repository/LaundryServiceRepository.java
+++ src/main/java/com/dhulai/repository/LaundryServiceRepository.java
@@ -8,11 +8,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dhulai.entity.LaundryShop;
 import com.dhulai.entity.LaundryShopServices;
+import com.dhulai.model.LaundryShopServicesId;
 
 
 
 // LaundryServiceRepository.java
-public interface LaundryServiceRepository extends JpaRepository<LaundryShopServices, Long> {
+public interface LaundryServiceRepository extends JpaRepository<LaundryShopServices, LaundryShopServicesId> {
     // Add custom queries if needed
     List<LaundryShopServices> findByLaundryShop(LaundryShop laundryShop);
     
