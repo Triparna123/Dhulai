b7a2ff2b5f6148e416af8eaf19a8774119e66ad1
Anindya Chakraborty <anindya100c@gmail.com>
2024-03-18 14:23:05 +0530

    Kaamchor Done

--- src/main/java/com/dhulai/repository/WorkingDaysAndTimeRepository.java
+++ src/main/java/com/dhulai/repository/WorkingDaysAndTimeRepository.java
@@ -5,10 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dhulai.entity.LaundryShop;
-import com.dhulai.entity.LaundryShopWorkingDaysAndTime;
-import com.dhulai.model.LaundryShopWorkingDaysAndTimeId;
+import com.dhulai.entity.WorkingDaysAndTime;
 
-public interface WorkingDaysAndTimeRepository extends JpaRepository<LaundryShopWorkingDaysAndTime,LaundryShopWorkingDaysAndTimeId>{
-    List<LaundryShopWorkingDaysAndTime> findByLaundryShop(LaundryShop laundryShop);
+public interface WorkingDaysAndTimeRepository extends JpaRepository<WorkingDaysAndTime,Integer>{
+    List<WorkingDaysAndTime> findByShopId(LaundryShop laundryShop);
 
 }
